5b5b4eddab2f7ecb5ebc50442336774df1df5057
sherbet-limon <davydova0909@gmail.com>
2025-04-11 13:55:37 +0300

    test BD ok

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
-	"github.com/sherbet-limon/final_practicum/pkg/server"
+	"go1f/pkg/server"
+	"go1f/pkg/db"
+	"log"
 )
 func main() {
+	if err:= db.Init("scheduler.db"); err != nil {
+		log.Printf("Ошибка инициализации БД: %v\n", err)
+	}
+	log.Println("БД успешно инициализирована")
 	server.Server()
+	
 }
--- pkg/db/db.go
+++ pkg/db/db.go
@@ -3,16 +3,43 @@ package db
 import ( 
 	"database/sql"
 	_ "modernc.org/sqlite"
+	"fmt"
+	"os"
+	"log"
 )
 	
 const schema = `
-CREATE TABL scheduler(
+CREATE TABLE IF NOT EXISTS scheduler(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 date CHAR(8) NOT NULL DEFAULT "",
 title VARCHAR(256) NOT NULL DEFAULT "",
 comment TEXT NOT NULL DEFAULT "",
-repeat VARCHAR(128) NOT NULL DEFAULT "",
+repeat VARCHAR(128) NOT NULL DEFAULT ""
 );`
 const schemaIdx = `
-CREATE INDEX date_id ON scheduler (date);`
+CREATE INDEX IF NOT EXISTS date_id ON scheduler (date);`
 
+//проверяем наличие бд, если нет, то CREATE TABLE INDEX
+func Init(dbFile string) error{
+	var install bool
+	_, err := os.Stat(dbFile)
+	if err != nil {
+		install = true
+	}
+	db, err := sql.Open("sqlite", dbFile)
+	if err != nil {
+        return fmt.Errorf("не удалось открыть БД: %w", err)
+    } 
+	defer db.Close()
+	if  install {
+		if _, err:= db.Exec(schema); err != nil {
+		//fmt.Errorf("не удалось создать таблицу: %w", err)
+		log.Println("не удалось создать таблицу: %w", err)
+		}
+		if _, err:= db.Exec(schemaIdx); err != nil {
+		//fmt.Errorf("не удалось создать индекс к date: %w", err)
+		log.Println("не удалось создать индекс к date: %w", err)
+		}
+	}
+	return nil
+	}
